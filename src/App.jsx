2cd2d3cbb1b64b08f4df96ca647365ae9191eeac
IEEEMOSTAFA <skmostafa8888@gmail.com>
2024-07-14 09:14:11 +0600

    total added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,51 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
 import Watch from './components/Watch/Watch'
 
 function App() {
-  const [count, setCount] = useState(0)
+//   const [count, setCount] = useState(0)
 
-  const watches = [
-    {Id: 1,name: 'Apple watch',price: 200},
-    {Id: 2,name: 'Samsung watch',price: 200},
-    {Id: 3,name: 'MI watch',price: 200},
-    {Id: 4,name: 'Pixel watch',price: 200},
-  ]
-
+//   const watches = [
+//     {Id: 1,name: 'Apple watch',price: 200},
+//     {Id: 2,name: 'Samsung watch',price: 200},
+//     {Id: 3,name: 'MI watch',price: 200},
+//     {Id: 4,name: 'Pixel watch',price: 200},
+//   ]
+  // const watches = [
+  //   {
+  //     "id": 1,
+  //     "name": "Apple Watch Series 8",
+  //     "price": 399
+  //   },
+  //   {
+  //     "id": 2,
+  //     "name": "Samsung Galaxy Watch 5",
+  //     "price": 279
+  //   },
+  //   {
+  //     "id": 3,
+  //     "name": "Garmin Forerunner 945",
+  //     "price": 599
+  //   },
+  //   {
+  //     "id": 4,
+  //     "name": "Fitbit Versa 3",
+  //     "price": 229
+  //   },
+  //   {
+  //     "id": 5,
+  //     "name": "Fossil Gen 6",
+  //     "price": 299
+  //   }
+  // ]
+  const [watches, setWatches] = useState([]);
+  useEffect( () =>{
+    fetch('watches.json')
+    .then(res => res.json())
+    .then(data => setWatches(data));
+  },[])
+  
   return (
     <>
       
